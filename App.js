f67b51f40acca52e96fc6f38e19f0c463964033e
Hari <hariharan.thulasiram@gmail.com>
2025-08-18 13:28:41 +0530

    code

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "this is h1"),
-    React.createElement("h2", {}, "this is h2"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "this is h1"),
-    React.createElement("h2", {}, "this is h2"),
-  ]),
+  React.createElement(
+    "div",
+    { id: "child", key: "child1" }, // ✅ added key
+    [
+      React.createElement("h1", { key: "h1_1" }, "this is h1"),
+      React.createElement("h2", { key: "h2_1" }, "this is h2"),
+    ]
+  ),
+  React.createElement(
+    "div",
+    { id: "child_2", key: "child2" }, // ✅ added key
+    [
+      React.createElement("h1", { key: "h1_2" }, "this is h1"),
+      React.createElement("h2", { key: "h2_2" }, "this is h2"),
+    ]
+  ),
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(parent);
