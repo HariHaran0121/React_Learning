08ea8977dda20a4851ba5ff0a825c388185c4678
Hari <hariharan.thulasiram@gmail.com>
2025-08-18 17:02:14 +0530

    Eposide-3

--- App.js
+++ App.js
@@ -1,24 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement(
-    "div",
-    { id: "child", key: "child1" }, // ✅ added key
-    [
-      React.createElement("h1", { key: "h1_1" }, "this is h1"),
-      React.createElement("h2", { key: "h2_1" }, "this is h2"),
-    ]
-  ),
-  React.createElement(
-    "div",
-    { id: "child_2", key: "child2" }, // ✅ added key
-    [
-      React.createElement("h1", { key: "h1_2" }, "this is h1"),
-      React.createElement("h2", { key: "h2_2" }, "this is h2"),
-    ]
-  ),
-]);
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    This is jsx tag
+  </h1>
+);
+
+const HeadingContainer = () => (
+<div id="container">
+  <Title/>
+  <h1 className="heading">This is functional component</h1>
+</div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingContainer/>);
